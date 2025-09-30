edff199f775ab31de1bc164c760fbd00d06cc4a6
Apham130 <andypham4747@gmail.com>
2025-09-30 17:50:36 +0000

    done its really elegant im very proud

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    lastFirstN("cream", "butter", 3);
+    stringManip("hello", "hello");
+    System.out.println(removeStr("badaboom", "ada"));
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +12,15 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    System.out.println(s1.substring(s1.length()-n) + s2.substring(s2.length()-n));
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    System.out.println(s1.toUpperCase());
+    System.out.println((s2.substring(0, 1)).toUpperCase() + (s2.substring(1)).toLowerCase());
   }
 
   /** Precondition: s1 and s2 are not null
@@ -25,7 +28,9 @@ public class Main
   */
   public static String removeStr(String s1, String s2)
   {
+    int x = s1.lastIndexOf(s2);
     String output = null;
+    output = (s1.substring(0,x) + s1.substring (x+s2.length()));
     return output;
   }
 }
